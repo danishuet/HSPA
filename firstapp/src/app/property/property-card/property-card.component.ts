bba2818d31a429c0ddc3e640a501fbf27141e4c2
danishuet <spober_woh@yahoo.com>
2024-01-21 19:49:28 +0500

    this is first commit

---
+++ firstapp/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'app-property-card',
+ 
+   templateUrl:'property-card.component.html',
+  
+   styleUrls:['property-card.component.css']
+
+}
+   
+)
+export class PropertyCardComponent
+{
+    Property:any={
+        "Id":1,
+        "Type":"House",
+        "Name":"Pakeeza Estate",
+        "Price":12000
+    }
+
+}
